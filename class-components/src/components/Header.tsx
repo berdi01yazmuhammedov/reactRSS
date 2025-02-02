1b12ddbc9612a6d1238665ee87fe5c9a0adae94a
Berdinazar <slybrd01@gmail.com>
2025-02-03 00:36:50 +0300

    feat: add Header

---
+++ class-components/src/components/Header.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+type State = {
+  searchTerm: string;
+};
+
+class Header extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      searchTerm: localStorage.getItem('searchTerm') || '',
+    };
+  }
+
+  handleSearch = () => {
+    const { searchTerm } = this.state;
+    localStorage.setItem('searchTerm', searchTerm.trim());
+    window.location.reload();
+  };
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <input
+          type="text"
+          value={this.state.searchTerm}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSearch}>Search</button>
+      </div>
+    );
+  }
+}
+
+export default Header;
