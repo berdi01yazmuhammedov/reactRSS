5bc5ae531388964d03d3403fb0b6b9921ce2244f
Berdinazar <slybrd01@gmail.com>
2025-02-10 02:03:00 +0300

    feat: add tests

---
+++ class-components/vitest.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'lcov'],
+      include: ['**/*.tsx'],
+      exclude: [
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+        'src/__tests__/setup.ts',
+        'src/App.tsx'
+      ],
+    },
+  },
+});
