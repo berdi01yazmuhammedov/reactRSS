1dea69265f8423f8f5776d82730b19774de9fe5b
Berdinazar <slybrd01@gmail.com>
2025-02-10 01:25:25 +0300

    refactor: change cardlist component

--- class-components/src/components/CardList.tsx
+++ class-components/src/components/CardList.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import Card from './Card';
+import { Item } from '../api';
 
-type Props = {
-  items: any[];
+type CardListProps = {
+  items: Item[];
+  onItemClick: (item: Item) => void;
 };
 
-const CardList: React.FC<Props> = ({ items }) => (
-  <div className="card-list">
-    {items.map((item, index) => (
-      <Card key={index} name={item.name} description={item.description} />
-    ))}
-  </div>
-);
+const CardList: React.FC<CardListProps> = ({ items, onItemClick }) => {
+  return (
+    <div className="card-list">
+      {items.map((item) => (
+        <div key={item.id} className="card" onClick={() => onItemClick(item)}>
+          {"login" in item ? (
+            <>
+              <img src={item.avatar_url} alt={item.login} width="50" />
+              <div>{item.login}</div>
+            </>
+          ) : (
+            <>
+              <h3>{item.name}</h3>
+              <p>{item.description}</p>
+            </>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default CardList;
