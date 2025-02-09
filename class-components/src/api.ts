e7adc653c4e7505b6c4311c10b5ee56456b52024
Berdinazar <slybrd01@gmail.com>
2025-02-10 01:13:59 +0300

    refactor: change swapi to api.github for pages

--- class-components/src/api.ts
+++ class-components/src/api.ts
@@ -1,12 +1,26 @@
-export const fetchItems = async (searchTerm: string) => {
+export interface Repository {
+  id: number;
+  name: string;
+  html_url: string;
+  description: string | null;
+}
+export interface GitHubUser {
+  id: number;
+  login: string;
+  avatar_url: string;
+}
+export type Item = Repository | GitHubUser;
+export const fetchItems = async (searchTerm: string): Promise<Item[]> => {
   const url = searchTerm
-    ? `https://api.github.com/users/${encodeURIComponent(searchTerm)}/repos?per_page=5&page=1`
+    ? `https://api.github.com/users/${encodeURIComponent(
+      searchTerm
+    )}/repos?per_page=5&page=1`
     : 'https://api.github.com/users';
 
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`Error ${response.status}: ${response.statusText}`);
   }
-  const data = await response.json();
-  return data.results;
+  const data: Item[] = await response.json();
+  return data;
 };
